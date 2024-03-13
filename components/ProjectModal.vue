d28d1550296c98a2d60c3278f360ddee0b080fe3
Baihaki Al Biruni <58780032+BaihakiTanjung@users.noreply.github.com>
2024-03-14 02:43:58 +0700

    Refactor ProjectModal.vue styling***

--- components/ProjectModal.vue
+++ components/ProjectModal.vue
@@ -1,16 +1,16 @@
 <template>
   <BaseModal v-if="data" v-bind="$attrs" v-on="$listeners">
     <!-- {{ data }} -->
-    <div class="text-center text-3xl font-bold">
+    <div class="text-3xl font-bold text-center">
       {{ data.name }}
     </div>
     <div class="my-3">
       <img :src="getPict(data.picture)" alt="card image" class="object-cover w-full h-64" />
     </div>
     <div class="me-3">
-      <a class="text-blue-400 font-bold" :href="data.url" target="_blank" rel="noopener noreferrer">Demo</a>
+      <a class="font-bold text-blue-400" :href="data.url" target="_blank" rel="noopener noreferrer">Demo</a>
     </div>
-    <div>
+    <div class="mb-12">
       <p>{{ data.desc }}</p>
     </div>
   </BaseModal>
